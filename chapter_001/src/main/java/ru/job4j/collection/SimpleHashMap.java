d4805770231038c479c67129b58a0933d7ff3881
konradmihelsson <konrad.mihelsson@gmail.com>
2020-06-19 16:31:13 +0400

    2. Уровень - Джуниор Блок 2.1. Структуры данных и алгоритмы. 5. Map 8. Реализовать собственную структуру данных - HashMap

---
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
@@ -0,0 +1,120 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleHashMap<K, V> implements Iterable<SimpleHashMap.Node<K, V>> {
+
+    private Node[] store;
+    private int modCount;
+    private int size;
+
+    public SimpleHashMap() {
+        this.store = new Node[1 << 2];
+    }
+
+    boolean insert(K key, V value) {
+        resizeIfNeed();
+        boolean result = false;
+        Node<K, V> node = new Node<>(key, value);
+        int index = indexFor(node.hash, this.store.length);
+        if (this.store[index] == null) {
+            this.store[index] = node;
+            result = true;
+            this.modCount++;
+            this.size++;
+        }
+        return result;
+    }
+
+    V get(K key) {
+        return (V) this.store[indexFor(hashCalc(key), this.store.length)].value;
+    }
+
+    boolean delete(K key) {
+        boolean result = false;
+        int index = indexFor(hashCalc(key), this.store.length);
+        if (this.store[index] != null) {
+            this.store[index] = null;
+            result = true;
+            this.modCount++;
+            this.size--;
+        }
+        return result;
+    }
+
+    private void resizeIfNeed() {
+        if (this.size == this.store.length) {
+            Node[] newSizeStore = new Node[this.store.length << 1];
+            for (Node node : this.store) {
+                newSizeStore[indexFor(node.hash, newSizeStore.length)] = node;
+            }
+            this.store = newSizeStore;
+        }
+    }
+
+    private static int hashCalc(Object key) {
+        int result = 0;
+        if (key != null) {
+            int h = key.hashCode();
+            result =  h ^ (h >>> 16);
+        }
+        return result;
+    }
+
+    private int indexFor(int hash, int length) {
+        return (length - 1) & hash;
+    }
+
+    @Override
+    public Iterator<Node<K, V>> iterator() {
+        return new Iterator<>() {
+            int position = 0;
+            int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                boolean result = false;
+                for (int i = position; !result && i < store.length; i++) {
+                    if (store[i] != null) {
+                        result = true;
+                        position = i;
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            public Node<K, V> next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return store[position++];
+            }
+        };
+    }
+
+    static class Node<K, V> {
+        private K key;
+        private V value;
+        private int hash;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.hash = hashCalc(key);
+        }
+
+        K getKey() {
+            return key;
+        }
+
+        V getValue() {
+            return value;
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/collection/SimpleHashMapTest.java
@@ -0,0 +1,88 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleHashMapTest {
+
+    @Test
+    public void whenAddThenGet() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        assertThat(map.insert(1, "first"), is(true));
+        assertThat(map.get(1), is("first"));
+    }
+
+    @Test
+    public void whenAddThenIt() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        assertThat(map.insert(1, "first"), is(true));
+        assertThat(map.iterator().next().getValue(), is("first"));
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void whenGetEmpty() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        map.get(1);
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenGetEmptyFromIt() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        map.iterator().next();
+    }
+
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        map.insert(1, "first");
+        Iterator<SimpleHashMap.Node<Integer, String>> it = map.iterator();
+        map.insert(2, "second");
+        it.next();
+    }
+
+    @Test
+    public void whenIncreaseStoreSize() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        map.insert(1, "first");
+        map.insert(2, "second");
+        map.insert(3, "third");
+        map.insert(4, "fourth");
+        map.insert(5, "fifth");
+        assertThat(map.get(5), is("fifth"));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenSameHash() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        map.insert(0, "zero");
+        map.insert(0, "zeroToo");
+        Iterator<SimpleHashMap.Node<Integer, String>> it = map.iterator();
+        it.next();
+        it.next();
+    }
+
+    @Test
+    public void whenDelete() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        map.insert(1, "first");
+        map.insert(2, "second");
+        assertThat(map.delete(2), is(true));
+        assertThat(map.delete(2), is(false));
+    }
+
+    @Test
+    public void whenAddNullKeyAndDeleteAll() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        map.insert(null, "first");
+        map.insert(null, "second");
+        assertThat(map.get(null), is("first"));
+        assertThat(map.delete(null), is(true));
+        assertThat(map.iterator().hasNext(), is(false));
+    }
+}
