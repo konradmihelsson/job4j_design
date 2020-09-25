41278e065f5a655a5b3ed5429def38c2853e1a2a
Konrad Mihelsson <konrad.mihelsson@gmail.com>
2020-09-25 19:32:52 +0300

    2. Уровень - Джуниор. Блок 2.2. Ввод-вывод. 1. Ввод-вывод. 4.1. Сканирование файловой системы. [#302107]

---
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,59 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, "js").forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(ext);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
+
+class SearchFiles implements FileVisitor<Path> {
+
+    SearchFiles(String ext) {
+        this.ext = ext;
+    }
+
+    private String ext;
+    private List<Path> store = new ArrayList<>();
+
+    List<Path> getPaths() {
+        return this.store;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (file.toFile().getName().endsWith(this.ext)) {
+            this.store.add(file);
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+}
