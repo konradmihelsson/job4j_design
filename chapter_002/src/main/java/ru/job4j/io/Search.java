c885b343cdd3037e7e40d4906d8d0976db36fbbc
Konrad Mihelsson <konrad.mihelsson@gmail.com>
2020-09-27 12:29:28 +0300

    2. Уровень - Джуниор. Блок 2.2. Ввод-вывод. 1. Ввод-вывод. 5. Валидация параметров запуска. [#302109]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -12,8 +12,11 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "js").forEach(System.out::println);
+        if (args.length == 0 || args.length > 2) {
+            throw new IllegalArgumentException("Wrong args. Usage: java -jar dir.jar ROOT_FOLDER FILE_EXTENSION");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
