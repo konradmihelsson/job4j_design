00090c106e7cae0028b7531e9a7c58730e36b9cf
Konrad Mihelsson <konrad.mihelsson@gmail.com>
2020-10-05 13:35:54 +0300

    2. Уровень - Джуниор. Блок 2.2. Ввод-вывод. 3. Логгирование. 3. Slf4j - вывод переменных. [#302104]

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte numOfRooms = 4;
+        short numOfFloors = 33;
+        int height = 109000;
+        char literal = 'D';
+        long totalArea = 262000000000L;
+        float distanceToRoad = 0.808f;
+        double distanceToCityCentre = 543275827788.34234234233333333;
+        boolean isMonolith = true;
+
+        LOG.debug("Number of rooms : {}, num of floors : {}, height in mm : {}"
+                + ", literal in address: {}, total area in square mm: {}, distance to road in km: {}"
+                + ", distance to centre of city: {}, building technology is monolith", numOfRooms,
+                numOfFloors, height, literal, totalArea, distanceToRoad, distanceToCityCentre, isMonolith);
     }
 }
