b607abc3d9b90ce55c609bf78434f41783c2385c
konradmihelsson <konrad.mihelsson@gmail.com>
2020-10-20 15:07:30 +0300

    2. Уровень - Джуниор. Блок 2.3. SQL, JDBC. 4. JDBC. 0. Видео [#302023] (добавил получение параметров из файла)

--- chapter_002/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ chapter_002/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,20 +1,28 @@
 package ru.job4j.jdbc;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/idea_db";
-        String login = "postgres";
-        String password = "password";
-        try (Connection connection = DriverManager.getConnection(url, login, password)) {
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getUserName());
-            System.out.println(metaData.getURL());
+        Properties config = new Properties();
+        try (InputStream in = ConnectionDemo.class.getClassLoader().getResourceAsStream("tracker.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Class.forName(config.getProperty("driver-class-name"));
+            try (Connection connection = DriverManager.getConnection(config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password"))) {
+                DatabaseMetaData metaData = connection.getMetaData();
+                System.out.println(metaData.getUserName());
+                System.out.println(metaData.getURL());
+            }
         }
     }
-}
