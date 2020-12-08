d27893eab46ad60ce861afa50f62312903bca93a
Konrad Mihelsson <konrad.mihelsson@gmail.com>
2020-12-08 16:37:52 +0300

    2. Уровень - Джуниор Блок 2.5. ООД. 0. TDD. 2. Что такое TDD? [#302131]

---
+++ chapter_002/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.tdd;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class CinemaTest {
+
+    @Ignore ("It use to study only")
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore ("It use to study only")
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Ignore ("It use to study only")
+    @Test
+    public void addTest() {
+        List<Session> sessions = new ArrayList<>();
+        Session session3d = new Session3D();
+        sessions.add(session3d);
+        assertThat(session3d, is(sessions.get(0)));
+    }
+}
