73a9ccf286af84f19cae2be86b90cc797d0b4d9f
konradmihelsson <konrad.mihelsson@gmail.com>
2020-12-10 09:01:45 +0300

    2. Уровень - Джуниор. Блок 2.5. ООД. 0. TDD. 1. Принципы Kiss, Dry и Yagni [#302129] (вынес дублирующий код в отдельный метод)

--- chapter_002/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_002/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,22 +2,21 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T result = null;
-        for (T t : value) {
-            if (comparator.compare(t, result) > 0) {
-                result = t;
-            }
-        }
-        return result;
+        return getByCondition(value, comparator, i -> i > 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
+        return getByCondition(value, comparator, i -> i < 0);
+    }
+
+    private <T> T getByCondition(List<T> value, Comparator<T> comparator, Predicate<Integer> predicate) {
         T result = null;
         for (T t : value) {
-            if (comparator.compare(t, result) < 0) {
+            if (predicate.test(comparator.compare(t, result))) {
                 result = t;
             }
         }
